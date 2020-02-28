773cd63e0025177a2b7d0e5ee131e2587518eb97
WilliamMyers <willy@wdmyers.co.uk>
2020-02-28 12:01:40 +0000

    FizzBuzz Game

--- src/main/java/com/myers/fizzbuzz/Main.java
+++ src/main/java/com/myers/fizzbuzz/Main.java
@@ -3,17 +3,17 @@ package com.myers.fizzbuzz;
 import com.myers.fizzbuzz.factory.FizzBuzzFactory;
 import com.myers.fizzbuzz.game.Game;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Game game = FizzBuzzFactory.getFizzBuzzGame();
-        System.out.println("Standard FizzBuzz Game...");
-        for(int i = 1; i<=100; i++){
-            System.out.println(game.play(i));
-        }
 
-        game = FizzBuzzFactory.getEnhancedFizzBuzzGame();
-        System.out.println("Enhanced FizzBuzz Game...");
+        Scanner stdin = new Scanner(System.in);
+        System.out.println("Please choose the game to play, 1 for standard, 2 for enhanced...\ninput:");
+        String flavour = stdin.nextLine();
+
+        Game game = flavour.equals("1")? FizzBuzzFactory.getFizzBuzzGame(): FizzBuzzFactory.getEnhancedFizzBuzzGame();
         for(int i = 1; i<=100; i++){
             System.out.println(game.play(i));
         }
